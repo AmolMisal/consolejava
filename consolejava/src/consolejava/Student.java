6867562bd652617417eeaaeae0c66dae4b0814bf
Amol <amol.misal1997@gmail.com>
2022-08-05 21:23:44 +0530

    New Class Updated

--- consolejava/src/consolejava/Student.java
+++ consolejava/src/consolejava/Student.java
@@ -5,6 +5,7 @@ public class Student {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hello Everyone");
+		System.out.println("Good morning");
 	}
 
 }
